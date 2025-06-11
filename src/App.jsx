b58185831b787138eb14c17b836c9efb7cef4d32
Vikash Kumawat <vvikash0012@gmail.com>
2025-06-11 23:15:30 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,128 @@
+import { useState, useEffect } from "react";
+import { Navbar } from "./components/navbar";
+import { parse, v4 as uuidv4 } from "uuid";
+
+import "./App.css";
+
+function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    let todoString = localStorage.getItem("todos");
+    if (todoString) {
+      let todos = JSON.parse(localStorage.getItem("todos"));
+      setTodos(todos);
+    }
+  }, []);
+
+  const saveToLS = (params) => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
+
+  const handleChange = (e) => {
+    setTodo(e.target.value);
+  };
+
+  const handleAdd = () => {
+    setTodos([...todos, { id: uuidv4(), todo, iscompleted: false }]);
+    setTodo("");
+    console.log(todos);
+    saveToLS();
+  };
+
+  const handleCheckbox = (e) => {
+    let id = e.target.name;
+    let index = todos.findIndex((item) => {
+      return item.id === id;
+      saveToLS();
+    });
+
+    let newTodos = [...todos];
+    newTodos[index].iscompleted = !newTodos[index].iscompleted;
+    setTodos(newTodos);
+  };
+
+  const handleEdit = (e, id) => {
+    let t = todos.filter((i) => i.id === id);
+    setTodo(t[0].todo);
+    let newTodos = todos.filter((item) => {
+      return item.id !== id;
+    });
+    setTodos(newTodos);
+    saveToLS();
+  };
+
+  const handleDelete = (e, id) => {
+    let newTodos = todos.filter((item) => {
+      return item.id !== id;
+    });
+    setTodos(newTodos);
+    saveToLS();
+  };
+
+  return (
+    <>
+      <Navbar />
+      <div className="container mx-auto bg-violet-200 my-5 rounded-xl p-5 min-h-[80vh]">
+        <div>
+          <h2 className="text-lg font-bold">Add a Todo</h2>
+          <input
+            onChange={handleChange}
+            value={todo}
+            type="text"
+            placeholder="Enter your todo"
+            className="w-1/2"
+          />
+          <button
+            onClick={handleAdd}
+            className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-6"
+          >
+            Save
+          </button>
+        </div>
+
+        <h2 className="text-lg font-bold">Your todos</h2>
+
+        <div className="todos">
+          {todos.length == 0 && <div className="m-3">No Todos to display</div>}
+          {todos.map((item, index) => {
+            return (
+              <div key={index} className="todo flex w-1/4 my-4 justify-between">
+                <div className="flex gap-4">
+                  <input
+                    name={item.id}
+                    onChange={handleCheckbox}
+                    type="checkbox"
+                    value={item.iscompleted}
+                    id=""
+                  />
+                  <div className={item.iscompleted ? "line-through" : ""}>
+                    {item.todo}
+                  </div>
+                </div>
+
+                <div className="buttons flex h-full">
+                  <button
+                    onClick={(e) => handleEdit(e, item.id)}
+                    className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
+                  >
+                    Edit
+                  </button>
+                  <button
+                    onClick={(e) => handleDelete(e, item.id)}
+                    className="bg-violet-800 hover:bg-violet-950 p-2 py-1 text-sm font-bold text-white rounded-md mx-1"
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Navbar = () => {
+  return (
+    <div>
+      <nav>
+        <ul className="flex">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact us</li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
